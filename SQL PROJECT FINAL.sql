f28ad59298bb4b330a1f8d4726be627322de0348
AsthaVaradkar <astha.varadkar25@gmail.com>
2024-12-27 19:05:51 +0530

    added all files

---
+++ SQL PROJECT FINAL.sql
@@ -0,0 +1,9 @@
+select * from [dbo].[constituencywise_details]; 
+
+select * from [dbo].[constituencywise_results];
+
+select * from [dbo].[partywise_results];
+
+select * from [dbo].[states];
+
+select * from [dbo].[statewise_results];
